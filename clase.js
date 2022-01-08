94ecbee09354d0fc4626d97cfab22c6e740f9ebb
Francisco Aguirre <franciscofaguirre@gmail.com>
2022-01-08 14:27:17 -0300

    primer commit

---
+++ clase.js
@@ -0,0 +1,36 @@
+let numeroA = 5;
+let numeroB = 8;
+
+const NUMEROC = 10;
+
+
+let resultadoSuma = numeroA + numeroB;
+
+
+console.log(resultadoSuma); 
+
+
+
+let textoA = "Coder";
+let textoB = "House";
+
+const BLANCO = " ";
+
+let resultado = textoA + textoB;
+let resultado2 = textoA + BLANCO + textoB;
+
+console.log(resultado); 
+console.log(resultado2);
+
+
+let nombreIngresado = prompt("Ingrese su nombre"); 
+let salida = nombreIngresado + " " + "ingresado.";
+
+console.log(salida);
+
+let numero1 = parseInt(prompt("Ingrese un número:"));
+let numero2 = parseInt(prompt("Ingrese otro número:"));
+
+let resultadoNumero = numero1 + numero2;
+
+alert ("El resultado de la suma es: " + resultadoNumero);
